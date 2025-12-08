da71d57676cccd640b473a7f8344b544f476e207
Vance Morris <vmorris@us.ibm.com>
2025-12-07 19:49:24 -0600

    day 7 p2 still broken, prepping for day 8

--- aoc2025/day07/solution.py
+++ aoc2025/day07/solution.py
@@ -1,4 +1,5 @@
 import copy
+from pprint import pprint
 import logging
 from aoc2025.util import get_input
 
@@ -8,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 class TachyonManifold:
     def __init__(self, data):
-        self.manifold = data
+        self.manifold = copy.deepcopy(data)
         self.row_index = 1
         self._start_beam()
 
@@ -18,7 +19,7 @@ class TachyonManifold:
     def _start_beam(self):
         index = self.get_start_index()
         _row = list(self.manifold[self.row_index])
-        _row[index] = "|"
+        _row[index] = 0
         self.manifold[self.row_index] = _row
 
     def step_beam(self):
@@ -28,21 +29,34 @@ class TachyonManifold:
         prev_row = list(self.manifold[self.row_index])
         self.row_index += 1
         _row = list(self.manifold[self.row_index])
+        logger.info(f"1. row_index: {self.row_index}, row: {_row}")
         for i, element in enumerate(_row):
-            if element == "^" and prev_row[i] == "|":
-                _row[i - 1] = "|"
-                _row[i + 1] = "|"
+            if element == "^" and type(prev_row[i]) == int:
+                if type(_row[i - 1]) == int:
+                    logger.info(
+                        f"  Found int on left side of split. index: {i-1}, element: {_row[i-1]}"
+                    )
+                    _row[i - 1] += prev_row[i]
+                    logger.info(f"2. row_index: {self.row_index}, row: {_row}")
+                else:
+                    _row[i - 1] = prev_row[i] + 1
+                    logger.info(f"3. row_index: {self.row_index}, row: {_row}")
+                _row[i + 1] = prev_row[i] + 1
+                logger.info(f"4. row_index: {self.row_index}, row: {_row}")
                 split_count += 1
-            if element == "." and prev_row[i] == "|":
-                _row[i] = "|"
+            if element == "." and type(prev_row[i]) == int:
+                _row[i] = prev_row[i]
+                logger.info(f"5. row_index: {self.row_index}, row: {_row}")
         self.manifold[self.row_index] = _row
         # time
         prev_row = list(self.manifold[self.row_index])
         self.row_index += 1
         _row = list(self.manifold[self.row_index])
+        logger.info(f"A. row_index: {self.row_index}, row: {_row}")
         for i, element in enumerate(_row):
-            if prev_row[i] == "|":
-                _row[i] = "|"
+            if type(prev_row[i]) == int:
+                _row[i] = prev_row[i]
+                logger.info(f"B. row_index: {self.row_index}, row: {_row}")
         self.manifold[self.row_index] = _row
 
         return split_count, timeline_count
@@ -50,7 +64,7 @@ class TachyonManifold:
     def __str__(self):
         result = "\n"
         for row in self.manifold:
-            result += "".join(row)
+            result += "".join([str(i) for i in row])
             result += "\n"
         return result
 
@@ -69,15 +83,24 @@ def solve_part1(entries):
 
 
 def solve_part2(entries):
-    manifold = TachyonManifold(entries)
+    manifold2 = TachyonManifold(entries)
     result = 0
+    for row_index, row in enumerate(manifold2.manifold):
+        _row = copy.copy(row)
+        for i, element in enumerate(_row):
+            if element == ".":
+                _row[i] = 0
+        manifold2.manifold[row_index] = _row
+
+    print(manifold2)
     try:
         while True:
-            _, timeline = manifold.step_beam()
+            _, timeline = manifold2.step_beam()
             result += timeline
     except IndexError:
         pass
-    logger.info(manifold)
+    print(manifold2)
+    # pprint(manifold.manifold)
     # 3125 too low - test passes
     # 4554 too low - test fails now (too high), 4555 is too low too
     return result
---
+++ aoc2025/day08/solution.py
@@ -0,0 +1,19 @@
+import logging
+from aoc2025.util import get_input
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def solve_part1(entries):
+    return
+
+
+def solve_part2(entries):
+    return
+
+
+if __name__ == "__main__":  # pragma: no cover
+    entries = get_input("aoc2025/day08/input")
+    print(solve_part1(entries))
+    print(solve_part2(entries))
